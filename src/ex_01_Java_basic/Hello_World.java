f8cab27c98cde569591a2129781d8d825e7c07f0
Kiran sable <kiransable@outlook.in>
2025-05-06 23:31:17 +0530

    1st program

--- src/ex_01_Java_basic/Hello_World.java
+++ src/ex_01_Java_basic/Hello_World.java
@@ -1,4 +1,9 @@
 package ex_01_Java_basic;
 
-public class Hello_World {
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" hi my name is kiran");
+    }
 }
