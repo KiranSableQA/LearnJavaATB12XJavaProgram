d9829decb797902ca91458c95599a4ac93117108
Kiran sable <kiransable@outlook.in>
2025-05-18 00:00:41 +0530

    17-05-2025 Five

---
+++ src/Task_01/EX03_Even_or_Odd.java
@@ -0,0 +1,11 @@
+package Task_01;
+
+public class EX03_Even_or_Odd {
+
+    public static void main(String[] args) {
+        int a =22;
+        int b=21;
+        System.out.println(a%2==0? "no is even" : "No is odd");
+        System.out.println(b%2==0? "no is even" : "No is odd");
+    }
+}
