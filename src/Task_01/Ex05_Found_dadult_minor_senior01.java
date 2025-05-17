b5929ea7fa31b410fc453b996c3565ccc467f8f2
Kiran sable <kiransable@outlook.in>
2025-05-18 00:00:11 +0530

    17-05-2025 Five

---
+++ src/Task_01/Ex05_Found_dadult_minor_senior01.java
@@ -0,0 +1,12 @@
+package Task_01;
+
+public class Ex05_Found_dadult_minor_senior01 {
+    public static void main(String[] args) {
+
+        int age=68;
+
+        String result = age>65?"Senior": (age<18?"Minor": "Adult");
+        System.out.println(result);
+
+    }
+}
