4695cbd944458c26438bbb5dc9af6eec9b4ca59b
Kiran sable <kiransable@outlook.in>
2025-05-25 21:56:08 +0530

    21-05-2025 p1

---
+++ src/ex_13_Functions/Lab128_Functions.java
@@ -0,0 +1,13 @@
+package ex_13_Functions;
+
+public class Lab128_Functions {
+    public static void main(String[] args) {
+// Built - in
+        // Created by the java Guys.
+        int result = Math.min(3,4);
+        int result_01 = Math.max(12,14);
+        System.out.println(result_01);
+        System.out.println(result);
+
+    }
+}
