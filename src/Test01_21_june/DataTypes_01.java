2dc8968a7781e832fcddff17ebf224f20de01c5a
Kiran sable <kiransable@outlook.in>
2025-06-21 12:04:45 +0530

    Merge remote-tracking branch 'origin/master'Test Java Programming Assessment01

---
+++ src/Test01_21_june/DataTypes_01.java
@@ -0,0 +1,20 @@
+package Test01_21_june;
+
+public class DataTypes_01 {
+
+    public static void main(String[] args)
+    {
+        int numberOfStudents = 150;
+        double averageScore = 87.5;
+        boolean hasPassedExam = false;
+        char firstInitial = 'J';
+        String studentName = "John Doe";
+
+        System.out.println("Number of Students: " + numberOfStudents);
+        System.out.println("Average Score: " + averageScore);
+        System.out.println("Has Passed Exam: " + hasPassedExam);
+        System.out.println("First Initial: " + firstInitial);
+        System.out.println("Student Name: " + studentName);
+    }
+    }
+
---
+++ src/Test01_21_june/PrintNumbers_11.java
@@ -0,0 +1,13 @@
+package Test01_21_june;
+
+public class PrintNumbers_11 {
+    public static void main(String[] args) {
+        int n = 5; // Hardcoded input
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
