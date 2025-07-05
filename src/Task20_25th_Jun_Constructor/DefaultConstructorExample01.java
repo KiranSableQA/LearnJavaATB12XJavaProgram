25bf3ee627005dbaf453a7c833693a305f2132d1
Kiran sable <kiransable@outlook.in>
2025-07-05 19:15:44 +0530

    Merge remote-tracking branch 'origin/master' Task 25th Jun. | Constructor

---
+++ src/Task20_25th_Jun_Constructor/DefaultConstructorExample01.java
@@ -0,0 +1,13 @@
+package Task20_25th_Jun_Constructor;
+
+public class DefaultConstructorExample01
+{
+    DefaultConstructorExample01() {
+        System.out.println("This is a default constructor.");
+    }
+
+    public static void main(String[] args) {
+        DefaultConstructorExample01 obj = new DefaultConstructorExample01();
+    }
+}
+
